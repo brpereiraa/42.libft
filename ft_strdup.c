9a5567060c659fe61d46350046f5b5a3ea0b8e67
Bruno Lopes <brunoo_miguel9@hotmail.com>
2023-03-08 16:51:52 +0000

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	dest = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
